c0851d4089b1371207c08de04a5af313ac0e9a1e
Diego Valbuena <divalbuenam@unal.edu.co>
2025-07-19 18:11:58 -0500

    Elimina la ruta de la página principal en server.js

--- server.js
+++ server.js
@@ -16,12 +16,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 const filesRouter = require('./routes/files');
 app.use('/api/files', filesRouter);
 
-
-// Página principal
-//app.get('/', (req, res) => {
-//  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-//});
-
 // Iniciar servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
