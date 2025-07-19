072103d05ba9c7a198be1c09acfa16c97a80d605
Diego Valbuena <divalbuenam@unal.edu.co>
2025-07-19 17:23:48 -0500

    "Corrige nombre de variable de puerto y agrega interfaz inicial"

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+
+// Leer .env si existe
 require('dotenv').config();
 
 console.log("Iniciando backend...");
 
-const express = require('express');
-const app = express();
+// Mostrar archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
-const PORT = 3000;
-
+// Rutas API
 const filesRouter = require('./routes/files');
 app.use('/api/files', filesRouter);
 
-const path = require('path');
 
+// Página principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
+// Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
