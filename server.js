0b541cf06863f59209715bc66945bae57b7664b8
Diego Valbuena <divalbuenam@unal.edu.co>
2025-07-19 17:55:20 -0500

    Corrige la ruta de las credenciales de Firebase en .env y comenta la ruta de la página principal en server.js

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use('/api/files', filesRouter);
 
 
 // Página principal
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+//app.get('/', (req, res) => {
+//  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//});
 
 // Iniciar servidor
 app.listen(port, () => {
